7ea553635add0f2ba5fae65f7235364a353f16ba
Noyan Alimov <noyan.alimov@gmail.com>
2020-05-24 07:06:23 +0100

    added webpack prod config file, edited server index.js file to serve static folder and respond to get request with an index.html file that was built in dist folder

--- src/server/index.js
+++ src/server/index.js
@@ -10,8 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile('dist/index.html');
+app.use(express.static(path.join(__dirname, '../../dist')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
 const textapi = new aylien({
